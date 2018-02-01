d226ebd132792299c1ec804aa42f4db4e668164d
grahamosh <grahamosh@outlook.com>
2018-02-01 20:38:45 +0000

    part 6 rgbgame

--- rgbgame.js
+++ rgbgame.js
@@ -13,6 +13,19 @@ var squares = document.querySelectorAll(".square");
 var pickedColor = pickColour();
 var colorDisplay = document.getElementById("colorDisplay");
 var messageDisplay = document.getElementById('message');
+var h1 = document.querySelector("h1");
+var buttonReset = document.getElementById('reset');
+
+buttonReset.addEventListener("click", function() {
+    colours = generateRandomColours(6);
+    pickedColor = pickColour();
+    colorDisplay.textContent = pickedColor;
+
+    for(var i = 0; i < squares.length; i++){
+        squares[i].style.background = colours[i];
+    }
+    h1.style.backgroundColor = "#232323";
+})
 
 colorDisplay.textContent = pickedColor;
 
@@ -27,6 +40,7 @@ for(var i = 0; i < squares.length; i++){
 		//compare color to pickedColor
 		if(clickedColor === pickedColor) {
 			messageDisplay.textContent = "Correct!";
+            buttonReset.textContent = "PLAY AGAIN?";
 			changeColours(clickedColor);
         } else {
 			messageDisplay.textContent = "Try Again!";
@@ -39,9 +53,26 @@ function changeColours (colour) {
     for(var i = 0; i < squares.length; i++){
         squares[i].style.backgroundColor = colour;
     }
+    h1.style.backgroundColor = colour;
 }
 
 function pickColour (num) {
     var randColour = Math.floor(Math.random() * colours.length);
     return colours[randColour];
+}
+
+function generateRandomColours(num) {
+    colArr = [];
+    for(var i = 0; i < num; i++){
+        var rgbStr = randomRGBColour();
+        colArr[i] = rgbStr;
+    }
+    return colArr;
+}
+
+function randomRGBColour() {
+    r = Math.floor(Math.random() * 256);
+    g = Math.floor(Math.random() * 256);
+    b = Math.floor(Math.random() * 256);
+    return rgb = 'rgb(' + r + ", " + g + ", " + b + ')';
 }
